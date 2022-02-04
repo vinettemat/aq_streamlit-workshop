f75e7bcb027c737b39efd9901925ee77be495ae6
vinettemat <48253592+vinettemat@users.noreply.github.com>
2022-02-04 16:01:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import streamlit as st
 from streamlit import write
 from transformers import pipeline
 import os
+from google.cloud import firestore
 
 def app():
-
+    db = firestore.Client.from_service_account_info(st.secrets["gcp_service_account"])
     st.write("This is my first app:") #Affichage d'un titre dans l'app
     unmasker = pipeline('fill-mask', model='distilbert-base-uncased')
     sentence = st.text_input('Fill in the sentence you want to try then press enter:', 'Data science is [MASK].')
